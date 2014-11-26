febc8d1e0bcbe023cc8a38c87dda4e8a84ec9fdd
Alexey Melnichuk <mimir@newmail.ru>
2014-11-26 13:00:44 +0400

    Fix. Build with Lua 5.3.beta

--- src/l52util.h
+++ src/l52util.h
@@ -4,6 +4,18 @@
 #include "lua.h"
 #include "lauxlib.h"
 
+#if LUA_VERSION_NUM >= 503 /* Lua 5.3 */
+
+#ifndef luaL_optint
+# define luaL_optint luaL_optinteger
+#endif
+
+#ifndef luaL_checkint
+# define luaL_checkint luaL_checkinteger
+#endif
+
+#endif
+
 #if LUA_VERSION_NUM >= 502 // lua 5.2
 
 // lua_rawgetp
